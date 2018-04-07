d86541ea3b9a1c38361c5d0bc7358b96971f743d
SignpostMarv <me@signpostmarv.name>
2018-04-07 09:54:06 +0100

    adjusting indentation

--- tests/DaftObjectRepositoryTest.php
+++ tests/DaftObjectRepositoryTest.php
@@ -38,56 +38,56 @@ class DaftObjectRepositoryTest extends TestCase
     protected function RepositoryDataProviderParams() : array
     {
         return [
-                [
-                    'Foo' => '1',
-                    'Bar' => 1.0,
-                    'Baz' => 1,
-                ],
-                [
-                    'Foo' => '2',
-                    'Bar' => 2.0,
-                    'Baz' => 2,
-                ],
-                [
-                    'Foo' => '3',
-                    'Bar' => 3.0,
-                    'Baz' => 3,
-                ],
-                [
-                    'Foo' => '4',
-                    'Bar' => 4.0,
-                    'Baz' => 4,
-                ],
-                [
-                    'Foo' => '5',
-                    'Bar' => 5.0,
-                    'Baz' => 5,
-                ],
-                [
-                    'Foo' => '6',
-                    'Bar' => 6.0,
-                    'Baz' => 6,
-                ],
-                [
-                    'Foo' => '7',
-                    'Bar' => 7.0,
-                    'Baz' => 7,
-                ],
-                [
-                    'Foo' => '8',
-                    'Bar' => 8.0,
-                    'Baz' => 8,
-                ],
-                [
-                    'Foo' => '9',
-                    'Bar' => 9.0,
-                    'Baz' => 9,
-                ],
-                [
-                    'Foo' => '10',
-                    'Bar' => 10.0,
-                    'Baz' => 10,
-                ],
+            [
+                'Foo' => '1',
+                'Bar' => 1.0,
+                'Baz' => 1,
+            ],
+            [
+                'Foo' => '2',
+                'Bar' => 2.0,
+                'Baz' => 2,
+            ],
+            [
+                'Foo' => '3',
+                'Bar' => 3.0,
+                'Baz' => 3,
+            ],
+            [
+                'Foo' => '4',
+                'Bar' => 4.0,
+                'Baz' => 4,
+            ],
+            [
+                'Foo' => '5',
+                'Bar' => 5.0,
+                'Baz' => 5,
+            ],
+            [
+                'Foo' => '6',
+                'Bar' => 6.0,
+                'Baz' => 6,
+            ],
+            [
+                'Foo' => '7',
+                'Bar' => 7.0,
+                'Baz' => 7,
+            ],
+            [
+                'Foo' => '8',
+                'Bar' => 8.0,
+                'Baz' => 8,
+            ],
+            [
+                'Foo' => '9',
+                'Bar' => 9.0,
+                'Baz' => 9,
+            ],
+            [
+                'Foo' => '10',
+                'Bar' => 10.0,
+                'Baz' => 10,
+            ],
         ];
     }
 
@@ -103,8 +103,8 @@ class DaftObjectRepositoryTest extends TestCase
             yield array_merge(
                 [
                     $className,
-                true,
-                true,
+                    true,
+                    true,
                 ],
                 $arrayParams
             );
