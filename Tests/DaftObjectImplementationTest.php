3e5ddc225d83f08aed5d90ee046b7ef13d24db40
SignpostMarv <me@signpostmarv.name>
2018-12-23 22:25:34 +0000

    php-cs-fixer

--- Tests/DaftObjectImplementationTest.php
+++ Tests/DaftObjectImplementationTest.php
@@ -1324,7 +1324,6 @@ class DaftObjectImplementationTest extends TestCase
             }
 
             if (isset($args[$setterProperty])) {
-
                 $obj->$setterProperty = $args[$setterProperty];
 
                 foreach ($propertiesExpectedToBeChanged as $property) {
