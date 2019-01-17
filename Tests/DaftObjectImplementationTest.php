d823f92358691f4615f5d2ef038723583e9b3e2f
SignpostMarv <me@signpostmarv.name>
2019-01-17 00:48:37 +0000

    updating testing

--- Tests/DaftObjectImplementationTest.php
+++ Tests/DaftObjectImplementationTest.php
@@ -1544,7 +1544,24 @@ class DaftObjectImplementationTest extends TestCase
             /**
             * @var array<int, string>
             */
-            $propertiesExpectedNotToBeChanged = [];
+            $propertiesExpectedNotToBeChanged = $className::DaftObjectProperties();
+
+            if (isset($otherProperties[$setterProperty])) {
+                $propertiesExpectedToBeChanged = $otherProperties[$setterProperty];
+                $propertiesExpectedNotToBeChanged = array_filter(
+                    $propertiesExpectedNotToBeChanged,
+                    function (string $maybe) use ($otherProperties, $setterProperty) : bool {
+                        return ! in_array($maybe, $otherProperties[$setterProperty], true);
+                    }
+                );
+            }
+
+            $propertiesExpectedNotToBeChanged = array_filter(
+                $propertiesExpectedNotToBeChanged,
+                function (string $maybe) use ($propertiesExpectedToBeChanged) : bool {
+                    return ! in_array($maybe, $propertiesExpectedToBeChanged, true);
+                }
+            );
 
             /**
             * @var array<int, string>
@@ -1553,17 +1570,6 @@ class DaftObjectImplementationTest extends TestCase
                 $propertiesExpectedToBeChanged,
                 $propertiesExpectedNotToBeChanged
             );
-
-            if (isset($otherProperties[$setterProperty])) {
-                /**
-                * @var array<int, string>
-                */
-                $propertiesExpectedToBeChanged = $otherProperties[$setterProperty];
-
-                if ( ! in_array($setterProperty, $propertiesExpectedToBeChanged, true)) {
-                    $propertiesExpectedNotToBeChanged[] = $setterProperty;
-                }
-            }
 
             foreach (
                 $checkingProperties as $property
@@ -1596,8 +1602,9 @@ class DaftObjectImplementationTest extends TestCase
                         ($className . '::$' . $property . ' should not be marked as changed.')
                     );
                 }
+            }
 
-                $obj->MakePropertiesUnchanged($setterProperty);
+                $obj->MakePropertiesUnchanged(...$checkingProperties);
 
                 foreach (
                     $checkingProperties as $property
@@ -1614,7 +1621,6 @@ class DaftObjectImplementationTest extends TestCase
                         )
                     );
                 }
-            }
         }
 
         /**
@@ -1622,8 +1628,23 @@ class DaftObjectImplementationTest extends TestCase
         */
         $obj = new $className([]);
 
+        $propertiesExpectedToBeChanged = [];
+
         foreach ($setters as $property) {
+            if ( ! isset($args[$property])) {
+                continue;
+            }
+
             $obj->$property = $args[$property];
+
+            if (isset($otherProperties[$property])) {
+                $propertiesExpectedToBeChanged = array_merge(
+                    $propertiesExpectedToBeChanged,
+                    $otherProperties[$property]
+                );
+            } else {
+                $propertiesExpectedToBeChanged[] = $property;
+            }
 
             if (in_array($property, $getters, true)) {
                 /**
@@ -1657,20 +1678,6 @@ class DaftObjectImplementationTest extends TestCase
         static::assertRegExp($regex, str_replace(["\n"], ' ', $debugInfo));
 
         foreach ($setters as $setterProperty) {
-            /**
-            * @var array<int, string>
-            */
-            $propertiesExpectedToBeChanged = [
-                $setterProperty,
-            ];
-
-            if (isset($otherProperties[$setterProperty])) {
-                /**
-                * @var array<int, string>
-                */
-                $propertiesExpectedToBeChanged = $otherProperties[$setterProperty];
-            }
-
             foreach ($propertiesExpectedToBeChanged as $property) {
                 static::assertTrue(
                     in_array($property, $obj->ChangedProperties(), true),
