3b7d6f58e81d00c72a6998ab4554f37e08e51244
SignpostMarv <me@signpostmarv.name>
2019-01-20 21:52:00 +0000

    using method reflection for interfaces

--- PHPStan/PropertyReflectionExtension.php
+++ PHPStan/PropertyReflectionExtension.php
@@ -167,6 +167,25 @@ class PropertyReflectionExtension implements PropertyReflection
     {
         if ( ! TypeParanoia::IsSubThingStrings($className, DaftObject::class)) {
             return self::BOOL_CLASS_NOT_DAFTOBJECT;
+        } elseif (interface_exists($className)) {
+            $getter = TypeUtilities::MethodNameFromProperty(
+                $property,
+                ! TypeUtilities::BOOL_DEFAULT_SET_NOT_GET
+            );
+            $setter = TypeUtilities::MethodNameFromProperty(
+                $property,
+                TypeUtilities::BOOL_DEFAULT_SET_NOT_GET
+            );
+
+            return
+                (
+                    method_exists($className, $getter) &&
+                    (new ReflectionMethod($className, $getter))->isPublic()
+                ) ||
+                (
+                    method_exists($className, $setter) &&
+                    (new ReflectionMethod($className, $setter))->isPublic()
+                );
         }
 
         return
