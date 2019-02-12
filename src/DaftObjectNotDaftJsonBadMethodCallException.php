842ae9c0f02fb3ecbdbf2728c1c61f7d5fbe1eef
SignpostMarv <me@signpostmarv.name>
2019-02-12 21:11:41 +0000

    adjusting indentation

--- src/DaftObjectNotDaftJsonBadMethodCallException.php
+++ src/DaftObjectNotDaftJsonBadMethodCallException.php
@@ -15,8 +15,11 @@ class DaftObjectNotDaftJsonBadMethodCallException extends BadMethodCallException
 {
     const INT_DEFAULT_CODE = 0;
 
-    public function __construct(string $class, int $code = self::INT_DEFAULT_CODE, Throwable $previous = null)
-    {
+    public function __construct(
+        string $class,
+        int $code = self::INT_DEFAULT_CODE,
+        Throwable $previous = null
+    ) {
         parent::__construct(($class . ' does not implement ' . DaftJson::class), $code, $previous);
     }
 }
