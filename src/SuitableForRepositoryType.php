5d281cab3f9c8605c8f4f6a8d3306887297a2923
SignpostMarv <me@signpostmarv.name>
2019-03-21 21:38:41 +0000

    updating packages & implementation

--- src/SuitableForRepositoryType.php
+++ src/SuitableForRepositoryType.php
@@ -8,6 +8,9 @@ declare(strict_types=1);
 
 namespace SignpostMarv\DaftObject;
 
+/**
+* @property mixed $id
+*/
 interface SuitableForRepositoryType extends
     DefinesOwnIdPropertiesInterface,
     DaftObjectCreatedByArray
